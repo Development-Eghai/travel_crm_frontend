d4705a05c34a06b7b490ec86b0b2cf61dc8fea78
krishna-kumarr <kumarkrishna11231@gmail.com>
2025-08-27 23:54:50 +0530

    gh pages setup - 3

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/my-vite-app/",
+  base: "/travel_crm_frontend/",
   plugins: [react()],
   optimizeDeps: {
     include: ['@mui/material', '@mui/x-data-grid']
